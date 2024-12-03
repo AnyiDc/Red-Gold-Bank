c0f508625e7dd89917b3b53c4a5a56ae60476e36
AnyiDc <anyi.katerine@hotmail.com>
2024-12-02 21:18:18 -0500

    agregando funcionalidad al formulario registro

--- views/Public/JS/scriptLogin.js
+++ views/Public/JS/scriptLogin.js
@@ -1,18 +1,18 @@
 
 function showForgetPassword() {
-    document.getElementById("Login").style.display = "none";
+    document.getElementById("login").style.display = "none";
     document.getElementById("forgetPassword").style.display = "block";
     document.getElementById("formulario_registro").style.display = "none";
 }
 
 function showformularioRegistro() {
-    document.getElementById("Login").style.display = "none";
+    document.getElementById("login").style.display = "none";
     document.getElementById("forgetPassword").style.display = "none";
     document.getElementById("formulario_registro").style.display = "block";
 }
 
 function showLogin() {
-    document.getElementById("Login").style.display = "block";
+    document.getElementById("login").style.display = "block";
     document.getElementById("forgetPassword").style.display = "none";
     document.getElementById("formulario_registro").style.display = "none";
 }
