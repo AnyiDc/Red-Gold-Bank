4f912d275d58b53b51f1cbb303b0741b38e23da0
Chala-java <chalajuandavid876@gmail.com>
2024-12-04 20:37:24 -0500

    se integro todos los proyectos Falta renombrar links

--- controllers/ControllerSoporte.js
+++ controllers/ControllerSoporte.js
@@ -1,61 +1,34 @@
-import { usuarios } from "../models/ModelUsuario.js";
+import { usuarios } from "../models/ModelSoporte.js";
 
-export function login() {
-  let loginUsuario = document.getElementById("nombreLogin").value;
-  let loginContrasena = document.getElementById("contraseniaLogin").value;
-  console.log(usuarios);
-  usuarios.some(function (index) {
-    // console.log(index)
-    if (loginUsuario == index.usuario && loginContrasena == index.contrasena) {
-      let timerInterval;
-      Swal.fire({
-        title: "Bienvenido: " + index.nombre,
-        html: "Será redireccionado en: <b></b> milliseconds.",
-        timer: 4000,
-        icon: "success",
-        timerProgressBar: true,
-        didOpen: () => {
-          Swal.showLoading();
-          const timer = Swal.getPopup().querySelector("b");
-          timerInterval = setInterval(() => {
-            timer.textContent = `${Swal.getTimerLeft()}`;
-          }, 100);
-        },
-        willClose: () => {
-          clearInterval(timerInterval);
-          window.location.href = "/views/pages/viewCredito.html";
-        },
-      }).then((result) => {
-        /* Read more about handling dismissals below */
-        if (result.dismiss === Swal.DismissReason.timer) {
-          console.log("I was closed by the timer");
-        }
-      });
-    } else {
-      Swal.fire({
-        title: "Error",
-        text: "Usuario y/contraseña incorrecto o no existe",
-        icon: "error",
-      });
-    }
+export function soporte() {
+  let registroNombre = document.getElementById("nombre").value.trim();
+  let registroCorreo = document.getElementById("email").value.trim();
+  let registroAsunto = document.getElementById("asunto").value.trim();
+
+  // Validar los datos ingresados contra los datos en el array
+  const encontrado = usuarios.some((usuario) => {
+    return (
+      registroNombre === usuario.nombre &&
+      registroCorreo === usuario.correo &&
+      registroAsunto === usuario.asunto
+    );
   });
-}
 
-export function register() {
-  let registroNombre = document.getElementById("registroNombre").value;
-  let registroDocumento = document.getElementById("registroDocumento").value;
-  let registroCorreo = document.getElementById("registroEmail").value;
-  let registroContrasena = document.getElementById("registroClave").value;
-  let registroConfirmar = document.getElementById("registroClave").value;
-  let newRegistro = {
-    nombre: registroNombre,
-    usuario: registroDocumento,
-    correo: registroCorreo,
-    contrasena: registroContrasena,
-    confirmar: registroConfirmar,
-  };
-  usuarios.unshift(newRegistro);
-  document.getElementById("formulario_registro").style.display = "none";
-  document.getElementById("Login").style.display = "flex";
-  console.log(usuarios);
+  if (encontrado) {
+    Swal.fire({
+      title: `Bienvenido, ${registroNombre}`,
+      text: "Validación exitosa.",
+      icon: "success",
+      timer: 3000,
+      showConfirmButton: false,
+    }).then(() => {
+      window.location.href = "/views/pages/viewCredito.html";
+    });
+  } else {
+    Swal.fire({
+      title: "Error",
+      text: "Datos incorrectos o no encontrados.",
+      icon: "error",
+    });
+  }
 }
